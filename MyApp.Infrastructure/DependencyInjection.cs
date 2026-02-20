212c3607db3bf4f108c0551cdaf1a31895ac3818
cyrus alde <12345678+aldecyrus425@users.noreply.github.com>
2026-02-20 20:06:01 +0800

    Initial Commit and Clean Archi Done Setup

---
+++ MyApp.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyApp.Domain.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureDI(this IServiceCollection service, IConfiguration configuration)
+        {
+            service.AddDbContext<ApplicationDBContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            return service;
+        }
+    }
+}
---
+++ MyApp.Infrastructure/Persistence/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.Domain.Persistence
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options) { }
+    }
+}
